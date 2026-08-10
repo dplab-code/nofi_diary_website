a840a9ff3be2278e67babaad6cf8a534b39a062a
Stefano Di Piazza <dr.stefano.dipiazza@gmail.com>
2026-08-10 11:55:45 +0200

    feat: add localized explore stories

---
+++ app/appearances/page.tsx
@@ -0,0 +1,3 @@
+import { ExplorePage, exploreMetadata } from "@/components/ExplorePages";
+export const metadata = exploreMetadata("en", "appearances");
+export default function Page(){ return <ExplorePage locale="en" page="appearances"/>; }
